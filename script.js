c5904af45190e4a6fb4c313a3420a6e04e6ddb6c
kwallity <41157146+kwallity@users.noreply.github.com>
2026-01-09 15:59:46 -0500

    Update script.js
    I think I got it with my fun facts.

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 const facts = [
-  "Penguins can drink seawater.",
-  "Penguins 'fly' underwater.",
-  "Emperor penguins can dive deep!",
   "African penguins are sometimes called jackass penguins because they make loud, donkey-like braying sounds to communicate with each other. These vocal calls help mates find one another and warn off rivals — pretty noisy for such cute birds.",
   "We, in IST, sometimes refer to grad students as African Penguins."
 ];
