86b09ba52f44886a7fdb7e8cccac7bd3d16bd542
kwallity <41157146+kwallity@users.noreply.github.com>
2026-01-09 15:56:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const facts = [
   "Penguins can drink seawater.",
   "Penguins 'fly' underwater.",
   "Emperor penguins can dive deep!",
-  "African penguins are sometimes called “jackass penguins” because they make loud, donkey-like braying sounds to communicate with each other. These vocal calls help mates find one another and warn off rivals — pretty noisy for such cute birds.",
-  "We, in IST, sometimes refer to grad students as \"African Penguins"\."
+  "African penguins are sometimes called jackass penguins because they make loud, donkey-like braying sounds to communicate with each other. These vocal calls help mates find one another and warn off rivals — pretty noisy for such cute birds.",
+  "We, in IST, sometimes refer to grad students as African Penguins."
 ];
 
 document.getElementById('factBtn').addEventListener('click', () => {
