9595269e259e807376f8b51a3c7e89fbfae31838
kwallity <41157146+kwallity@users.noreply.github.com>
2026-01-09 14:22:46 -0500

    Update script.js
    trying to get my fun fact in there correctly.

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
 const facts = [
-"African penguins are sometimes called “jackass penguins” because they make loud, donkey-like braying sounds to communicate with each other!
-These vocal calls help mates find one another and warn off rivals — pretty noisy for such cute birds. We in IST sometimes refer to grad students as "African Penguins"... for very different reasons."
-//  "Penguins can drink seawater.",
-//  "Penguins 'fly' underwater.",
-//  "Emperor penguins can dive deep!"
-];
+  "African penguins are sometimes called \“jackass penguins”\ because they make loud, donkey-like braying sounds to communicate with each other. These vocal calls help mates find one another and warn off rivals — pretty noisy for such cute birds. We, in IST, sometimes refer to grad students as \"African Penguins"\... for very different reasons.",
+  ];
 
 document.getElementById('factBtn').addEventListener('click', () => {
   const i = Math.floor(Math.random() * facts.length);
