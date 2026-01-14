b733cc8772f7a871bac3f40d86b8a589e4bc1bcc
kwallity <41157146+kwallity@users.noreply.github.com>
2026-01-14 09:45:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const facts = [
   'African penguins are sometimes called "jackass penguins" because they make loud, donkey-like braying sounds to communicate with each other. These vocal calls help mates find one another and warn off rivals — pretty noisy for such cute birds.',
-  'Coincidentally... We, in IST, sometimes refer to grad students as "African Penguins".'
+  'We, in IST, sometimes refer to grad students as "African Penguins".'
 ];
 
 document.getElementById('factBtn').addEventListener('click', () => {
